84670725e6a887edc702133e8281d9156a45d5a4
himanshu saini <himanshusaini97511@gmail.com>
2025-04-02 20:35:21 +0530

    secound comment

---
+++ resources/views/pages/career.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title', 'Home Page')
+
+@section('content')
+
+
+
+
+
+
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\CorporateServicesController;
+use App\Http\Controllers\PaymentsController;
+use App\Http\Controllers\CareerController;
+use App\Http\Controllers\ReferController;
+use App\Http\Controllers\WhoweareController;
+use App\Http\Controllers\SucessStoriesController;
+
+
 
 
 
@@ -27,6 +36,14 @@ Route::middleware('auth')->group(function () {
 Route::get('/about', [AboutController::class, 'index'])->name('about');
 Route::get('/contact', [ContactController::class, 'index'])->name('contact');
 Route::get('/blog', [BlogController::class, 'index'])->name('blog');
+Route::get('/course', [CourseController::class, 'index'])->name('course');
+Route::get('/corporate_services', [CorporateServicesController::class, 'index'])->name('corporate_services');
+Route::get('/payments', [PaymentsController::class, 'index'])->name('payments');
+Route::get('/career', [CareerController::class, 'index'])->name('career');
+Route::get('/refer_and_earn', [ReferController::class, 'index'])->name('refer_and_earn');
+Route::get('/who_we_are', [WhoweareController::class, 'index'])->name('who_we_are');
+Route::get('/success_stories', [SucessStoriesController::class, 'index'])->name('success_stories');
+
 
 
 require __DIR__.'/auth.php';
