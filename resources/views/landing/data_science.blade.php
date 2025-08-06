531740358dd03ca11df6a641666f62302f9c782a
Himanshu <himanshusaini97511@gmail.com>
2025-08-06 14:12:57 +0530

    add form updateion

--- resources/views/landing/data_science.blade.php
+++ resources/views/landing/data_science.blade.php
@@ -1441,11 +1441,27 @@
         <div class="rrr">
             <div class="form-container" id="callForm">
                 <h4>Request a Callback</h4>
-                <input type="text" placeholder="Your Name" required>
-                <input type="email" placeholder="Your Email" required>
-                <input type="tel" placeholder="Your Phone" required>
-                <br>
-                <button class="close-form-btn" id="closeFormBtn">Close</button>
+                <form id="callbackForm" method="post" action="https://demo.digicrome.com/post_lead.php">
+                    @csrf
+            <!-- Visible Fields -->
+            <input type="text" name="name" placeholder="Your Name" required>
+            <input type="email" name="email" placeholder="Your Email" required>
+            <input type="tel" name="mobile" placeholder="Your Phone" required>
+
+            <!-- Hidden Fields -->
+            <input type="hidden" name="title" value="">
+            <input type="hidden" name="address" value="">
+            <input type="hidden" name="profession" value="">
+            <input type="hidden" name="source" value="">
+            <input type="hidden" name="country" value="">
+            <input type="hidden" name="comp_name" value="">
+            <input type="hidden" name="state" value="">
+            <input type="hidden" name="altr_mobile" value="">
+
+            <br>
+            <button class="close-form-btn" id="closeFormBtn" type="button">Close</button>
+            <button type="submit" class="submit-btn">Submit</button>
+        </form>
             </div>
         </div>
 
@@ -1535,32 +1551,9 @@
                         <div class="modal-content">
                             <span class="close-btn" style="font-size: 22px;" onclick="closeEnrollModal()">&times;</span>
                             <div class="form-container">
-                                <div class="form-heading">
-                                    <div class="row">
-                                        <div class="col-lg-8">
-                                            <h4
-                                                style="text-align: left; color:#fff; font-weight: 700;    line-height: 1; padding-top: 10px;">
-                                                Request Callback</h4>
-                                            <p style="text-align: left; color:#fff;">Fill out the form below!</p>
-
-                                        </div>
-                                        <div class="col-lg-4" style="margin-top: 7px;     margin-bottom: 3%; ">
-                                            <div class="bbx"
-                                                style="background-color: #fff; border-radius: 26px; width: 54%; margin-left: 41%; height: 82%;;">
-                                                <svg style="    margin-top: 15px;" xmlns="http://www.w3.org/2000/svg"
-                                                    width="37" height="26" fill="currentColor"
-                                                    class="bi bi-telephone-fill" viewBox="0 0 16 16">
-                                                    <path fill-rule="evenodd"
-                                                        d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"
-                                                        style="
-                                                    color: #f29c12;
-                                                " />
-                                                </svg>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                            
                                 <form action="https://demo.digicrome.com/post_lead.php" method="post">
+                                    @csrf
                                     <input type="hidden" name="_token"
                                         value="rw1IePlAcOc1qGaBjZLwHSoR2hyn67owyCNWa6Mc">
                                     <div class="form-group">
@@ -1650,6 +1643,11 @@
                                 closeEnrollModal();
                             }
                         }
+                           window.onload = function() {
+        // Open either of the modals on page load (uncomment the one you need)
+        openModal(); // This will open the myModal
+        // openEnrollModal(); // This will open the enrollModal
+    }
                     </script>
                     <script>
                         const newDate = new Date('08-10-2025 09:00:00').getTime();
@@ -1837,6 +1835,7 @@
                                 </div>
                             </div>
                             <form action="https://demo.digicrome.com/post_lead.php" method="post">
+                                @csrf
                                 <input type="hidden" name="_token"
                                     value="rw1IePlAcOc1qGaBjZLwHSoR2hyn67owyCNWa6Mc">
                                 <div class="form-group">
@@ -2834,6 +2833,7 @@
                                         </div>
                                     </div>
                                     <form action="https://demo.digicrome.com/post_lead.php" method="post">
+                                        @csrf
                                         <input type="hidden" name="_token"
                                             value="rw1IePlAcOc1qGaBjZLwHSoR2hyn67owyCNWa6Mc">
                                         <div class="form-group">
