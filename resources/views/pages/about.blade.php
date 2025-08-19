a3fd100488a7ee60a56f91729174b094db2ad0c5
Himanshu <himanshusaini97511@gmail.com>
2025-08-19 12:29:25 +0530

    add nunmber remove image

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -29,9 +29,9 @@
 				</div>
 			</div>
 		</div>
-		<div class="breadcumb-shape">
+		{{-- <div class="breadcumb-shape">
 			<img loading="lazy" src="{{ asset('assets/images/inner-img/breadcumb-dot.webp') }}" alt="dot" title="dot">
-		</div>
+		</div> --}}
 		<div class="breadcumb-shape2">
 			<img loading="lazy" src="{{ asset('assets/images/inner-img/breadcumb-ball.webp') }}" alt="ball" title="ball">
 		</div>
