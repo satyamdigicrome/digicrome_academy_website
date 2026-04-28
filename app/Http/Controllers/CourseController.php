b46d650828f9dc67bfb922f1d1da1071a8f08967
Satyam Shukla <satyam.digicrome@gmail.com>
2026-04-28 17:34:54 +0530

    changes complete

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -55,18 +55,22 @@ class CourseController extends Controller
     $courses = Cache::remember('latest_courses', 60, function () {
         return Course::latest()->take(3)->get(['id', 'name', 'slug', 'image', 'tag_line']);
     });
-
-    $placements = Cache::remember('placements', 60, function () use ($course) {
-        if($course->course_free == 1) {
-            return Placement::where('category', 'DS')->get(['id', 'name', 'position', 'package', 'image']);
-        } elseif($course->course_free == 2) {
-            return Placement::where('category', 'IB')->get(['id', 'name', 'position', 'package', 'image']);
-        } elseif($course->course_free == 3) {
-            return Placement::where('category', 'AISS')->get(['id', 'name', 'position', 'package', 'image']);
-        }else{
+    $cacheKey = 'placements_course_' . $course->course_free;
+    $placements = Cache::remember($cacheKey, 60, function () use ($course) {
+        if ($course->course_free == 1) {
+            return Placement::where('category', 'DS')
+                ->get(['id', 'name', 'position', 'package', 'image']);
+        } elseif ($course->course_free == 2) {
+            return Placement::where('category', 'IB')
+                ->get(['id', 'name', 'position', 'package', 'image']);
+        } elseif ($course->course_free == 3) {
+            return Placement::where('category', 'AISS')
+                ->get(['id', 'name', 'position', 'package', 'image']);
+        } else {
             return Placement::all(['id', 'name', 'image']);
         }
     });
+
     $companyLogos = Cache::remember('company_logos', 60, function () {
         return Logo::where('type', 'companies')->get(['id', 'image']);
     });
