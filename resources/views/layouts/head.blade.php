3b546072ef0f13d4ad7d9b1eef661df0514be524
Satyam Shukla <satyam.digicrome@gmail.com>
2026-07-27 16:42:35 +0530

    changes complete

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -132,6 +132,13 @@
     </noscript>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" media="print"
         onload="this.media='all'">
+    <script type="text/javascript">
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "w08flkqvsv");
+    </script>
     <script type="application/ld+json" >
       {
         "@context": "https://schema.org",
