2806eb5d574def7dd08f61d9b3dc713e798b4c55
Satyam Shukla <satyam.digicrome@gmail.com>
2026-07-30 16:02:10 +0530

    changes complete

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -32,7 +32,13 @@
         crossorigin="anonymous">
     <link rel="preload" href="{{ asset('assets/css/fonts/memvYaGs126MiZpBA-UvWbX2vVnXBbObj2OVTS-muw.woff2') }}"
         as="font" type="font/woff2" crossorigin="anonymous">
-
+    <script type="text/javascript">
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "w08flkqvsv");
+    </script>
     @php
         /*
          * Layout-critical CSS. These MUST be render-blocking: loading them
