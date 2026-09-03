21eca6c58b46fdf2d5df31f1aaf672f44b317359
Satyam Shukla <satyam.digicrome@gmail.com>
2026-09-03 14:14:53 +0530

    changes complete

--- resources/views/pages/thankyou.blade.php
+++ resources/views/pages/thankyou.blade.php
@@ -32,6 +32,13 @@ fbq('track', 'PageView');
 <noscript><img height="1" width="1" style="display:none"
 src="https://www.facebook.com/tr?id=874727045237562&ev=PageView&noscript=1"
 /></noscript>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MLQN7DF5');</script>
+<!-- End Google Tag Manager -->
 @endpush
 
 @section('content')
@@ -56,5 +63,8 @@ src="https://www.facebook.com/tr?id=874727045237562&ev=PageView&noscript=1"
     </div> --}}
   
   </div>
-  
+  <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MLQN7DF5"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 @endsection
